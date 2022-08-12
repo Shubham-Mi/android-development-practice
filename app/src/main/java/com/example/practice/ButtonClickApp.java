809801864f494fa9d447c82bbe1588ad73afbbfc
Shubham Mittal2 <shubham.mittal2@ibm.com>
2022-08-12 15:24:46 +0530

    Save Text App and Logging

---
+++ app/src/main/java/com/example/practice/ButtonClickApp.java
@@ -0,0 +1,95 @@
+package com.example.practice;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class ButtonClickApp extends AppCompatActivity {
+    private EditText userInput;
+    private TextView textView;
+    private static final String TAG = "ButtonClickApp";
+    private final String TEXT_CONTENTS = "TextContents";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate: in");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.button_click_layout);
+        userInput = findViewById(R.id.buttonClickEditText);
+        Button button = findViewById(R.id.buttonClickButton);
+        textView = findViewById(R.id.buttonCLickTextView);
+        textView.setText("");
+        userInput.setText("");
+        textView.setMovementMethod(new ScrollingMovementMethod());
+        button.setOnClickListener(view -> {
+            String result = userInput.getText().toString() + "\n";
+            userInput.setText("");
+            textView.append(result);
+        });
+        Log.d(TAG, "onCreate: out");
+    }
+
+    @Override
+    protected void onStart() {
+        Log.d(TAG, "onStart: in");
+        super.onStart();
+        Log.d(TAG, "onStart: out");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        Log.d(TAG, "onRestoreInstanceState: in");
+        super.onRestoreInstanceState(savedInstanceState);
+        textView.setText(savedInstanceState.getString(TEXT_CONTENTS));
+        Log.d(TAG, "onRestoreInstanceState: out");
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d(TAG, "onResume: in");
+        super.onResume();
+        Log.d(TAG, "onResume: out");
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d(TAG, "onPause: in");
+        super.onPause();
+        Log.d(TAG, "onPause: out");
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        Log.d(TAG, "onSaveInstanceState: in");
+        outState.putString(TEXT_CONTENTS, textView.getText().toString());
+        super.onSaveInstanceState(outState);
+        Log.d(TAG, "onSaveInstanceState: out");
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d(TAG, "onStop: in");
+        super.onStop();
+        Log.d(TAG, "onStop: out");
+    }
+
+    @Override
+    protected void onRestart() {
+        Log.d(TAG, "onRestart: in");
+        super.onRestart();
+        Log.d(TAG, "onRestart: out");
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d(TAG, "onDestroy: in");
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: out");
+    }
+}
